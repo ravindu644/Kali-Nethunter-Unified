0c1b67bee790a801cc001490ced01600173a77f3
ravindu644 <droidcasts@protonmail.com>
2025-12-18 12:12:12 +0530

    service^Cdynamically update the firmware_class path

--- kernelsu-module/common/service.sh
+++ kernelsu-module/common/service.sh
@@ -7,3 +7,28 @@
 MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
+
+# Configure firmware path
+if [ -f "/sys/module/firmware_class/parameters/path" ]; then
+    current_path=$(cat /sys/module/firmware_class/parameters/path 2>/dev/null)
+    new_path=""
+
+    # Add /vendor/firmware if not present
+    if [ -d "/vendor/firmware" ] && ! echo "$current_path" | grep -q "/vendor/firmware"; then
+        new_path="/vendor/firmware"
+    fi
+
+    # Add chroot firmware path if exists
+    if [ -L "${NHSYS}/kalifs" ]; then
+        chroot_fw="$(readlink -f ${NHSYS}/kalifs)/lib/firmware"
+        if [ -d "$chroot_fw" ] && ! echo "$current_path" | grep -q "$chroot_fw"; then
+            [ -n "$new_path" ] && new_path="$new_path,$chroot_fw" || new_path="$chroot_fw"
+        fi
+    fi
+
+    # Update firmware path if we have new entries
+    if [ -n "$new_path" ]; then
+        [ -n "$current_path" ] && final_path="$current_path,$new_path" || final_path="$new_path"
+        echo "$final_path" > /sys/module/firmware_class/parameters/path 2>/dev/null
+    fi
+fi
